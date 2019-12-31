98945a1121d603063e50c5399a359d19be5d0e36
yanyan nie <yanyan.nie@etu.utc.fr>
2019-12-31 15:34:29 +0800

    mod part3-2

---
+++ 11-sed进阶/05-模式替代.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# &符号，代表替换命令中的匹配模式，不管预定义模式是什么文本，都可以用and符号替换
+# &符号会提取匹配替换命令中指定模式的整个字符串
+echo "The cat sleeps in his hat" | sed 's/.at/"&"/g'
+
+# 替换单独的单词
+echo "The System Administrator manual" | sed 's/\(System\) Administrator/\1 user/'
+
+# 在长数字中插入逗号
+echo "1234567" | sed '{:start; s/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/; t start}'
+
